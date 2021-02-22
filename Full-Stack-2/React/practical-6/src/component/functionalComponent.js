692098c22d77d568a7cfb2dfbc2fff801c0fc0db
Jayant Dwivedi <jayantdwivedi19@gmail.com>
2021-02-22 22:02:46 +0530

    adding functional component

--- Full-Stack-2/React/practical-6/src/component/functionalComponent.js
+++ Full-Stack-2/React/practical-6/src/component/functionalComponent.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './functionalComponent.css';
 
-function myfunctionalComponent(props) {
+function FunctionalComponent(props) {
     return (
-        <div>
-            <h1>This is functional react component</h1>
+       <>
+        <div id ="root-function">
+            <h1>This is functional Component</h1>
+            <h4> <span className = "red-function">created by: </span> <span className="blue-function">{props.name}</span></h4>
         </div>
+       </>
     );
-};
+}
 
-export default myfunctionalComponent;
+export default FunctionalComponent;

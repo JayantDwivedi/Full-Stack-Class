76c3123662f2a6251561835d596845b96f2fa698
Jayant Dwivedi <jayantdwivedi19@gmail.com>
2021-02-27 15:37:30 +0530

    props with proper object structure

--- Full-Stack-2/React/practice/src/index.js
+++ Full-Stack-2/React/practice/src/index.js
@@ -41,55 +41,99 @@ import "./index.css";
 // with nesting of other components
 
 // passing props in from of object
-const thirdBook = {
-  title: "One Arranged Marrige",
-  author: "Chetan Bhagat",
-  image:
-    "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages-na.ssl-images-amazon.com%2Fimages%2FI%2F61r-WomIz0L.jpg&imgrefurl=https%3A%2F%2Fwww.amazon.in%2FOne-Arranged-Murder-Chetan-Bhagat%2Fdp%2F1542094135&tbnid=fRPXZJQpXytgdM&vet=12ahUKEwju_Zqag4jvAhUYCXIKHdcmAAAQMygAegUIARCbAQ..i&docid=qLGWBD9qaIBoXM&w=660&h=1057&q=one%20arranged%20marriage&ved=2ahUKEwju_Zqag4jvAhUYCXIKHdcmAAAQMygAegUIARCbAQ",
-};
+// const thirdBook = {
+//   title: "One Arranged Marrige",
+//   author: "Chetan Bhagat",
+//   image:
+//     "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages-na.ssl-images-amazon.com%2Fimages%2FI%2F61r-WomIz0L.jpg&imgrefurl=https%3A%2F%2Fwww.amazon.in%2FOne-Arranged-Murder-Chetan-Bhagat%2Fdp%2F1542094135&tbnid=fRPXZJQpXytgdM&vet=12ahUKEwju_Zqag4jvAhUYCXIKHdcmAAAQMygAegUIARCbAQ..i&docid=qLGWBD9qaIBoXM&w=660&h=1057&q=one%20arranged%20marriage&ved=2ahUKEwju_Zqag4jvAhUYCXIKHdcmAAAQMygAegUIARCbAQ",
+// };
 
 // use of javascript jsx
-const title = "Book 1";
-const author = "Mr. X";
-const img =
-  "https://m.media-amazon.com/images/I/91CQ7MCHIfL._AC_UY327_FMwebp_QL65_.jpg";
+// const title = "Book 1";
+// const author = "Mr. X";
+// const img =
+//   "https://m.media-amazon.com/images/I/91CQ7MCHIfL._AC_UY327_FMwebp_QL65_.jpg";
+
+// creating an object and pass into props
+// after making this we just simply add data into this objects
+const books = [
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51RyuFPuxjL._AC_SX184_.jpg",
+    title: "Young Gift and Black",
+    author: "Jamia Wilson",
+  },
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51zaYM+my1L._AC_SX184_.jpg",
+    title: "Antiracist Baby Picture",
+    author: "Ibram X. Kendi Ashley",
+  },
+  {
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51Tlm0GZTXL._AC_SX184_.jpg",
+    title: "Atomic Habits",
+    author: "James Clear",
+  },
+];
 
 function MiniBook() {
   return (
     <div>
-      <Book image={img} title="Book Another" />
+      {/* <Book image={img} title="Book Another" />
       <Book image={img} title="Book Previous" author="Jayant Dwivedi" />
       <Book
         title={thirdBook.title}
         image={thirdBook.image}
         author={thirdBook.author}
-      />
+      /> */}
 
       {/* Adding this book has description others don't 
       with the use of props children  we use opening and closing tags instead of self closing*/}
-      <Book title={thirdBook.title} image={img} author={thirdBook.author}>
+      {/* <Book title={thirdBook.title} image={img} author={thirdBook.author}>
         <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore
           facilis, nobis ea aperiam in suscipit laboriosam nulla animi accusamus
           eveniet.
         </p>
-      </Book>
+      </Book> */}
+      {/* calling names from names list  */}
+      {/* <h4>Simple list objects</h4>
+      <h4>{names}</h4>
+
+      <h4>List names one by one</h4>
+      {newName} */}
+
+      {/* using of object as props  */}
+      {books.map((book) => {
+        const { img, title, author } = book;
+        return <Book book={book}></Book>;
+      })}
     </div>
   );
 }
 
+// Try to render simple list on our webpage
+// const names = ["Jayant", "Kartik", "Harsh", "Hitesh"];
+
+// create a map function to fetch single single values from list
+// notice the return name and argument passed
+// const newName = names.map((name) => {
+//   return <h1>{name}</h1>;
+// });
+
 // Main Component
 const Book = (props) => {
+  console.log(props);
+  const { img, title, author } = props.book;
   return (
-    <article className="booklist">
-      <img src={props.image} alt="image of a book" />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
-      <h4>Sadguru</h4>
-
-      {/* name is special  */}
-      {props.children}
-    </article>
+    <>
+      <article className="booklist">
+        <img src="{img}" />
+        <h1>{title}</h1>
+        <h4>{author}</h4>
+      </article>
+    </>
   );
 };
 
